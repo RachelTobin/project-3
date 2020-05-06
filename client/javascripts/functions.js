2a1d7277a070d3ae72ea9c64bc4dd9ecc9f295a9
Rachel Tobin <rtobin@uoregon.edu>
2020-05-06 14:38:07 -0700

    Add Req. 4b to functions.js

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -12,37 +12,40 @@ let arrOf1To100 = fillArray(1, 100, 1);
 //log the array for inspection
 console.log(arrOf1To100);
 
+
+
+//Req. 4b
 let bitBatBotOrNot = (n) => {
   let result = "";
-
   // is n divisible by 3?
   if (n % 3 == 0) {
     result = `${result}Bit`;
   }
-
   // is n divisible by 5?
   if (n % 5 == 0) {
     result = `${result}Bat`;
   }
-
   //is n divisible by 7?
   if (n % 7 == 0) {
     result = `${result}Bot`;
   }
-
   //if n is not divisible by 3, 5 or 7
-  else {
-    result = `${result}Not`;
+  if (n % 3 != 0 && n % 5 != 0 && n % 7 != 0) {
+    result = "Not";
   }
-
   return result;
 };
 
+
+
+//Req. 4c
 let findAllbitBatBotOrNots1 = (arr) => {
-  //your code goes here
-  //use map method
+
 };
 
+
+
+//Re
 let findAllbitBatBotOrNots2 = (arr) => {
   //your code goes here
   //use for statement
